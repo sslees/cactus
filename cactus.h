3a8aceed20f4d62edefddfbfcf8776520a43b8fb
sslees <sam@sslees.com>
2016-12-06 22:10:35 -0800

    change sample size for demo

--- cactus.h
+++ cactus.h
@@ -21,7 +21,7 @@
 #define PAYLOAD_LEN 16
 #define BUFF_LEN 255
 #define S_BETWEEN_UPDATES 5
-#define SAMPLE_SIZE 12
+#define SAMPLE_SIZE 3
 #define DRY_THRESHOLD 30.0
 #define WATERED_THRESHOLD 45.0
 
