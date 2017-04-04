90f59f5f8fc677f51a2056cb0414c69fcba82212
sslees <sam@sslees.com>
2017-04-04 03:36:29 +0000

    minor bug

--- client.py
+++ client.py
@@ -37,6 +37,6 @@ while True:
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    s.connect((HOST, PORT))
    s.sendall(data)
-   s.shutdown(SHUT_RDWR)
+   s.shutdown(socket.SHUT_RDWR)
    s.close()
    time.sleep(UPDATE_INTERVAL)
