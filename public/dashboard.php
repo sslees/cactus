7e154db2ba1043b4146aa017a8607c76d4d22505
sslees <sam@sslees.com>
2017-05-19 08:58:35 +0000

    file cleanup

--- public/dashboard.php
+++ public/dashboard.php
@@ -13,7 +13,7 @@ References:
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="phase2/assets/css/main.css" />
+  <link rel="stylesheet" href="assets/css/main.css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <style>
